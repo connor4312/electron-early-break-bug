4cb72826656729bed10618096606e9b3efdfb2bb
Connor Peet <connor@peet.io>
2023-12-08 15:18:52 -0800

    init

---
+++ preload.mjs
@@ -0,0 +1,12 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+import { contextBridge, ipcRenderer } from 'electron/renderer';
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  makeNewWindow: () => ipcRenderer.send('make-new-window')
+})
